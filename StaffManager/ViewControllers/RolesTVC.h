d81c8f7347133f44bce44b9d298cb8aa625e391d
Dejan Kumric <tetigs@beotel.rs>
2015-01-31 23:08:27 +0100

    Pocetni projekat za CoreData primer sa sajta Tim Roadley. (Tim Roadely konsultacija).

---
+++ StaffManager/ViewControllers/RolesTVC.h
@@ -0,0 +1,13 @@
+//
+//  RolesTVC.h
+//  StaffManager
+//
+//  Created by Dejan Kumric on 1/31/15.
+//  Copyright (c) 2015 Dejan Kumric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RolesTVC : UITableViewController
+
+@end
